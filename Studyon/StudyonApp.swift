4c9e6b8819cbec7d87119a16641488297df46b6f
Daniel Moreno <danmore@nmsu.edu>
2025-02-13 23:56:59 -0700

    Initial Commit

---
+++ Studyon/StudyonApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyonApp.swift
+//  Studyon
+//
+//  Created by Daniel Moreno on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct StudyonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyonTests/StudyonTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyonTests.swift
+//  StudyonTests
+//
+//  Created by Daniel Moreno on 2/13/25.
+//
+
+import Testing
+@testable import Studyon
+
+struct StudyonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
